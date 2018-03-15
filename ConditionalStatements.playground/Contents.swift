2c5c4baa81e8a43a2cb0ff454b5d09b0751b08d3
Adegoke Obasa <adegokeobasa@gmail.com>
2018-03-15 16:01:21 +0100

    feat: add more love score conditions

--- ConditionalStatements.playground/Contents.swift
+++ ConditionalStatements.playground/Contents.swift
@@ -3,9 +3,12 @@ import UIKit
 func loveCalculator(yourName: String, theirName: String) -> String {
     let loveScore : Int = Int(arc4random_uniform(101))
     if loveScore > 80 {
-        return "You love each other like Kanye loves Kanye"
-    } else {
-        return "No love possible, you'll forever be alone"
+        return "Your love score is \(loveScore). You love each other like Kanye loves Kanye"
+    } else if loveScore > 40 && loveScore <= 80 {
+        return "Your love score is \(loveScore). You go together like coke and mentos"
+    }
+    else {
+        return "Your love score is \(loveScore). No love possible, you'll forever be alone"
     }
 }
 
